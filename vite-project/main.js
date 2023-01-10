b6028f516fafb141c0d80b7f7c3e890ee762b8d4
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-01-10 13:13:35 -0500

    1/10

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,25 @@
 const URL = "https://uselessfacts.jsph.pl/random";
-async function getFact(URL) {
-  const response = await fetch(URL);
-  console.log(response);
+console.log(fetch(URL));
+
+fetch(URL)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+async function fetchdata(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 }
+fetchdata(URL);
+
+const apiResponseDOM = document.getElementById("api-response");
+const putQuoteInHTML = async () => {
+  const fact = await fetchdata(URL);
+  apiResponseDOM.innerHTML = `Fact: ${fact}`;
+};
+putQuoteInHTML();
