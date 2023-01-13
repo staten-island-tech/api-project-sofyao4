1ee96a9cb22e1ed7979c78c9339eb5b8df904dd5
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-01-13 13:14:15 -0500

    1/13

--- vite-project/main.js
+++ vite-project/main.js
@@ -13,6 +13,6 @@ async function fetchData(url) {
 const apiResponseDOM = document.getElementById("api-response");
 const putJokeInHTML = async () => {
   const joke = await fetchData(url);
-  apiResponseDOM.innerHTML = `Joke: ${joke.value} Category: ${joke.categories}`;
+  apiResponseDOM.innerHTML = `Joke: ${joke.value} <img src="${joke.icon_url}">`;
 };
 putJokeInHTML();
