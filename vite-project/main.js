7231ae3bf594f01ba980603618ff9cc54058a567
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-01-20 13:12:39 -0500

    1/20

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,18 +1,36 @@
-const url = "https://api.chucknorris.io/jokes/random";
+// const url = "https://api.chucknorris.io/jokes/random";
 
-async function fetchData(url) {
+// async function fetchData(url) {
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+// const apiResponseDOM = document.getElementById("api-response");
+// const putJokeInHTML = async () => {
+//   const joke = await fetchData(url);
+//   apiResponseDOM.innerHTML = `Joke: ${joke.value} <img src="${joke.icon_url}">`;
+// };
+// putJokeInHTML();
+
+const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+async function fetchData(URL) {
   try {
-    const response = await fetch(url);
+    const response = await fetch(URL);
     const data = await response.json();
     console.log(data);
-    return data;
   } catch (err) {
     console.error(err);
   }
 }
 const apiResponseDOM = document.getElementById("api-response");
-const putJokeInHTML = async () => {
-  const joke = await fetchData(url);
-  apiResponseDOM.innerHTML = `Joke: ${joke.value} <img src="${joke.icon_url}">`;
+const logPoke = async () => {
+  const poke = await fetchData(URL);
+  apiResponseDOM.insertAdjacentHTML("beforeend", `Name: ${poke.name}`);
 };
-putJokeInHTML();
+logPoke();
