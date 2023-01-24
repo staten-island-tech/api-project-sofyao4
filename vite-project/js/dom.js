bc6e319e54db84dd23fd7aa95961e50e2dbc6799
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-01-24 17:59:02 -0500

    1/24

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,8 @@
-DOM = {
+const DOM = {
   submitName: document.querySelector("#submit-btn"),
   apiResponseDOM: document.getElementById("api-response"),
+  form: document.getElementById("form"),
+  input: document.getElementById("input"),
+  button: document.getElementById("button"),
 };
+export { DOM };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,20 +1,52 @@
 import "../styles/main.css";
-const url = "https://api.disneyapi.dev/character?";
-async function fetchData(url) {
+import { DOM } from "./dom.js";
+
+async function fetchData() {
+  const value = DOM.input.value;
+  console.log(value);
+  const url = `https://api.disneyapi.dev/character?name=${value}`;
+  DOM.apiResponseDOM.innerHTML = "";
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
+    if (response.status < 200 || response.status > 299) {
+      console.log(response.status);
+      DOM.apiResponseDOM.insertAdjacentHTML("afterbegin", `Cant find`);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      function cardMaker(element) {
+        DOM.apiResponseDOM.insertAdjacentHTML(
+          "beforeend",
+          `<div>
+    <h2 class="cardName">Name:${element.name}</h2>
+    <img src=${element.imageUrl}>
+    </div>`
+        );
+      }
+      data.data.forEach((element) => cardMaker(element));
+
+      DOM.input.value = "";
+    }
   } catch (err) {
     console.error(err);
   }
 }
 
-DOM.submitName.addEventListener("submit", function () {});
+// const getChar = async () => {
+//   const char = await fetchData(url);
+//   char.data.forEach((element) => console.log(element.name));
 
-const getChar = async () => {
-  const char = await fetchData(url);
-  DOM.apiResponseDOM.innerHTML = `Name: ${char.name} <img src=${char.imageUrl}>`;
-};
-getChar();
+//   function cardMaker(element) {
+//     DOM.apiResponseDOM.insertAdjacentHTML(
+//       "beforeend",
+//       `Name: ${element.name} <img src=${element.imageUrl}>`
+//     );
+//   }
+//   char.data.forEach((element) => cardMaker(element));
+// };
+// getChar();
+
+DOM.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  fetchData();
+});
