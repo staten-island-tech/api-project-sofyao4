f580826427aa784dcaa0391f0b41da70ea7e6bc2
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2023-01-24 23:09:57 -0500

    1/24

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,52 +1,40 @@
 import "../styles/main.css";
 import { DOM } from "./dom.js";
 
-async function fetchData() {
-  const value = DOM.input.value;
-  console.log(value);
-  const url = `https://api.disneyapi.dev/character?name=${value}`;
-  DOM.apiResponseDOM.innerHTML = "";
-  try {
-    const response = await fetch(url);
-    if (response.status < 200 || response.status > 299) {
-      console.log(response.status);
-      DOM.apiResponseDOM.insertAdjacentHTML("afterbegin", `Cant find`);
-    } else {
-      const data = await response.json();
-      console.log(data);
-      function cardMaker(element) {
-        DOM.apiResponseDOM.insertAdjacentHTML(
-          "beforeend",
-          `<div>
-    <h2 class="cardName">Name:${element.name}</h2>
-    <img src=${element.imageUrl}>
-    </div>`
-        );
+const myFunctions = {
+  cardMaker: function (element) {
+    DOM.apiResponseDOM.insertAdjacentHTML(
+      "beforeend",
+      `<div class="element-card">
+  <h2 id="card-name">${element.name}</h2>
+  <img id="card-img" src=${element.imageUrl}>
+  
+  </div>`
+    );
+  },
+  fetchData: async function () {
+    const value = DOM.input.value;
+    console.clear();
+    const url = `https://api.disneyapi.dev/character?name=${value}`;
+    DOM.apiResponseDOM.innerHTML = "";
+    try {
+      const response = await fetch(url);
+      if (response.status < 200 || response.status > 299) {
+        console.log(response.status);
+        DOM.apiResponseDOM.insertAdjacentHTML("afterbegin", `Cant find`);
+      } else {
+        const data = await response.json();
+        console.log(data);
+        data.data.forEach((element) => myFunctions.cardMaker(element));
+        DOM.input.value = "";
       }
-      data.data.forEach((element) => cardMaker(element));
-
-      DOM.input.value = "";
+    } catch (err) {
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-// const getChar = async () => {
-//   const char = await fetchData(url);
-//   char.data.forEach((element) => console.log(element.name));
-
-//   function cardMaker(element) {
-//     DOM.apiResponseDOM.insertAdjacentHTML(
-//       "beforeend",
-//       `Name: ${element.name} <img src=${element.imageUrl}>`
-//     );
-//   }
-//   char.data.forEach((element) => cardMaker(element));
-// };
-// getChar();
+  },
+};
 
 DOM.form.addEventListener("submit", function (e) {
   e.preventDefault();
-  fetchData();
+  myFunctions.fetchData();
 });
